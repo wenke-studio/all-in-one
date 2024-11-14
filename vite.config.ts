74d800e877f4e52e84745b0e1e2c1d254bfd2d5d
wenke.lai <wenke.studio@gmail.com>
2024-11-14 21:25:49 +0800

    feat: remove this project created by vite

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import { defineConfig } from "vite";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-});
