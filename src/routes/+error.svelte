76ce1ecc3c589abefb54de2f7f21024603baa6b8
wenke.lai <wenke.studio@gmail.com>
2024-11-16 11:05:32 +0800

    feat: add a error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { goto } from "$app/navigation";
+  import { page } from "$app/stores";
+  import { Heading, P } from "flowbite-svelte";
+  import { onMount } from "svelte";
+
+  let countdown = 5;
+
+  onMount(() => {
+    const interval = setInterval(() => {
+      countdown -= 1;
+      if (countdown <= 0) {
+        clearInterval(interval);
+        goto("/");
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
+</script>
+
+<div class="">
+  <Heading>{$page.status}: {$page.error?.message}</Heading>
+  <P>Redirecting in {countdown} seconds...</P>
+</div>
