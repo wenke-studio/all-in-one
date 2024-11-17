a9ab7152c1f35453d25521a1041ac0fa6f5f0143
wenke.lai <wenke.studio@gmail.com>
2024-11-17 15:49:10 +0800

    fix(dashboard): add user info to sidebar CTA

--- src/routes/dashboard/Sidebar.svelte
+++ src/routes/dashboard/Sidebar.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
   import { page } from "$app/stores";
   import { title } from "$lib/stores/app";
+  import { authStore } from "$lib/stores/auth";
   import {
+    Avatar,
+    Button,
+    P,
     Sidebar,
     SidebarBrand,
+    SidebarCta,
     SidebarGroup,
     SidebarItem,
     SidebarWrapper,
@@ -33,12 +38,21 @@
 </script>
 
 <Sidebar activeUrl={$page.url.pathname}>
-  <SidebarWrapper class="h-full border-r border-gray-700">
+  <SidebarWrapper class="h-full border-r border-gray-700 flex flex-col">
     <SidebarBrand {site} imgClass="ml-2 h-6 me-3 sm:h-7 bg-gray-300 rounded" />
-    <SidebarGroup>
+    <SidebarGroup class="flex-grow">
       {#each routes as { label, href }}
         <SidebarItem {label} {href} />
       {/each}
     </SidebarGroup>
+    <SidebarCta
+      spanClass="hidden"
+      class="bg-green-400 flex flex-col middle button"
+    >
+      <Avatar src={$authStore.user?.image} />
+      <P>{$authStore.user?.username}</P>
+      <P class="opacity-60">{$authStore.user?.email}</P>
+      <Button class="mt-6" disabled>Upgrade to Pro</Button>
+    </SidebarCta>
   </SidebarWrapper>
 </Sidebar>
