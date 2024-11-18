fe0fb3fec1ad1ec61c412e1e60aa2513d8a559f7
wenke.lai <wenke.studio@gmail.com>
2024-11-18 10:05:36 +0800

    feat(clerk): add sign-in and sign-out buttons

---
+++ src/lib/clerk/SignInButton.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Button } from "flowbite-svelte";
+  import clerk from "./store";
+
+  const signIn = () => {
+    $clerk?.openSignIn();
+  };
+</script>
+
+<Button on:click={signIn}>
+  <slot>Sign In</slot>
+</Button>
---
+++ src/lib/clerk/SignOutButton.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Button } from "flowbite-svelte";
+  import clerk from "./store";
+
+  const signOut = () => {
+    $clerk?.signOut();
+  };
+</script>
+
+<Button on:click={signOut}>
+  <slot>Sign out</slot>
+</Button>
