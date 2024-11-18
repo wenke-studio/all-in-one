482bc89fb9ac52393072a037362d02763d0ec62b
wenke.lai <wenke.studio@gmail.com>
2024-11-18 12:07:51 +0800

    fix(clerk): add props to sign-in-button to control actions

--- src/lib/clerk/SignInButton.svelte
+++ src/lib/clerk/SignInButton.svelte
@@ -2,11 +2,44 @@
   import { Button } from "flowbite-svelte";
   import clerk from "./store";
 
+  interface Props {
+    // show sign-in modal or redirect to custom sign-in page
+    mode?: "modal" | "redirect" | undefined;
+
+    // redirect to this url after sign-in
+    afterSignInUrl?: string | undefined;
+
+    // redirect to this url after sign-up
+    afterSignUpUrl?: string | undefined;
+
+    // only used when mode is "redirect" and custom sign-in page
+    redirectUrl?: string | undefined;
+
+    children?: () => any;
+  }
+
+  const {
+    afterSignInUrl,
+    afterSignUpUrl,
+    redirectUrl,
+    mode = "modal",
+    children,
+  }: Props = $props();
+
   const signIn = () => {
-    $clerk?.openSignIn();
+    const opts = { afterSignInUrl, afterSignUpUrl, redirectUrl };
+    if (mode === "modal") {
+      $clerk?.openSignIn(opts);
+    } else {
+      $clerk?.redirectToSignIn(opts);
+    }
   };
 </script>
 
 <Button on:click={signIn}>
-  <slot>Sign In</slot>
+  {#if children}
+    {@render children()}
+  {:else}
+    Sign In
+  {/if}
 </Button>
